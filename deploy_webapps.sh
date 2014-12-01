682512085a337eb4f60a132e6530894e98c8f684
Zhanhui Li <lizhanhui@gmail.com>
2014-12-01 11:54:13 +0800

    Update document

---
+++ deploy_webapps.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp rocketmq-cockpit/target/rocketmq-cockpit-3.2.2.war ~/jetty/webapps/
+cp rocketmq-cockpit/doc/rocketmq-cockpit-3.2.2.xml ~/jetty/webapps/
+
+cp rocketmq-console/target/rocketmq-console-3.2.2.war ~/jetty/webapps/
+cp rocketmq-console/doc/rocketmq-console-3.2.2.xml ~/jetty/webapps/
