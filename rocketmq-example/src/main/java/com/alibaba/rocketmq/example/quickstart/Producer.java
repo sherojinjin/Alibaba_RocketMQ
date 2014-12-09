e892c0eb9267a8a75a85f433d23dd4f751309d64
Zhanhui Li <lizhanhui@gmail.com>
2014-12-10 00:07:06 +0800

    Update example.

--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/quickstart/Producer.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/quickstart/Producer.java
@@ -28,13 +28,13 @@ import com.alibaba.rocketmq.common.message.Message;
  */
 public class Producer {
     public static void main(String[] args) throws MQClientException, InterruptedException {
-        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        DefaultMQProducer producer = new DefaultMQProducer("PG_QuickStart");
         producer.setSendMsgTimeout(10000);
         producer.start();
 
         for (int i = 0; i < 4; i++) {
             try {
-                Message msg = new Message("TopicTest_Lien",// topic
+                Message msg = new Message("T_QuickStart",// topic
                     "TagA",// tag
                     ("Hello RocketMQ " + i).getBytes()// body
                         );
