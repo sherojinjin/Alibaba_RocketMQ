7dcc5e349c163c2f5a87e1bcca7a40116b1c174c
Zhanhui Li <lizhanhui@gmail.com>
2014-12-10 15:50:45 +0800

    Update example to benchmark the system.

--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/concurrent/Producer.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/concurrent/Producer.java
@@ -3,17 +3,23 @@ package com.alibaba.rocketmq.example.concurrent;
 import com.alibaba.rocketmq.client.producer.concurrent.MultiThreadMQProducer;
 import com.alibaba.rocketmq.common.message.Message;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Producer {
 
+    private static final AtomicLong SEQUENCE_GENERATOR = new AtomicLong(0L);
+
     public static void main(String[] args) {
-        int count = 1000;
+        int count = 0;
         if (args.length > 0) {
             count = Integer.parseInt(args[0]);
+        } else {
+            count = -1;
         }
         AtomicLong successCount = new AtomicLong(0L);
-        MultiThreadMQProducer producer = MultiThreadMQProducer.configure()
+        final MultiThreadMQProducer producer = MultiThreadMQProducer.configure()
                 .configureProducerGroup("PG_QuickStart")
                 .configureCorePoolSize(200)
                 .configureConcurrentSendBatchSize(100)
@@ -21,17 +27,30 @@ public class Producer {
                 .configureSendMessageTimeOutInMilliSeconds(3000)
                 .configureDefaultTopicQueueNumber(16)
                 .build();
-
                 producer.registerCallback(new ExampleSendCallback(successCount, count, System.currentTimeMillis()));
-
-        Message[] messages = new Message[count];
-
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < count; i++) {
-            messages[i] = new Message("T_QuickStart", "Test MultiThread message".getBytes());
+        if (count < 0) {
+            Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    Message[] messages = buildMessages(3000);
+                    producer.send(messages);
+                }
+            }, 0, 1000, TimeUnit.MILLISECONDS);
+        } else {
+            long start = System.currentTimeMillis();
+            Message[] messages = buildMessages(count);
+            producer.send(messages);
+            System.out.println("Messages are sent in async manner. Cost " + (System.currentTimeMillis() - start) + "ms");
         }
-        producer.send(messages);
-
-        System.out.println("Messages are sent in async manner. Cost " + (System.currentTimeMillis() - start) + "ms");
     }
+
+    public static Message[] buildMessages(int n) {
+        Message[] messages = new Message[n];
+        for (int i = 0; i < n; i++) {
+            messages[i] = new Message("T_QuickStart", "Test MultiThread message".getBytes());
+            messages[i].putUserProperty("sequenceId", String.valueOf(SEQUENCE_GENERATOR.incrementAndGet()));
+        }
+        return messages;
+    }
+
 }
