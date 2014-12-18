7a95074eea1e42d2063e40a32125cdefd9a91eb9
Li Zhanhui <lizhanhui@gmail.com>
2014-12-18 15:12:04 +0800

    Make shutdown method public.

--- rocketmq-client/src/main/java/com/alibaba/rocketmq/client/consumer/cacheable/CacheableConsumer.java
+++ rocketmq-client/src/main/java/com/alibaba/rocketmq/client/consumer/cacheable/CacheableConsumer.java
@@ -177,7 +177,7 @@ public class CacheableConsumer
      * This method shuts down this client properly.
      * @throws InterruptedException If unable to shut down within 1 minute.
      */
-    private void shutdown() throws InterruptedException {
+    public void shutdown() throws InterruptedException {
         //Stop pulling messages from server.
         defaultMQPushConsumer.shutdown();
 
