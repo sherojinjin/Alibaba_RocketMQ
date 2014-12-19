e96c80381c8cb90c3c66533ba66d6e9c79d626b5
Li Zhanhui <lizhanhui@gmail.com>
2014-12-19 18:59:14 +0800

    Debug. Unfinished work.

--- rocketmq-remoting/src/main/java/com/alibaba/rocketmq/remoting/netty/SslHelper.java
+++ rocketmq-remoting/src/main/java/com/alibaba/rocketmq/remoting/netty/SslHelper.java
@@ -1,7 +1,12 @@
 package com.alibaba.rocketmq.remoting.netty;
 
 import com.alibaba.rocketmq.remoting.exception.SSLContextCreationException;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
@@ -15,6 +20,10 @@ public class SslHelper {
     private static final String DEFAULT_KEY_STORE_PASSWORD = "changeit";
 
     private static final String DEFAULT_KEY_PASSWORD = "changeit";
+
+    private static final String INITIAL_VECTOR = "0102030405060708";
+
+    private static final String CIPHER = "AES/CBC/PKCS5Padding";
 
     public static SSLContext getSSLContext(SslRole role) throws SSLContextCreationException {
         try {
@@ -84,6 +93,47 @@ public class SslHelper {
         return engine;
     }
 
+    public static String encrypt(String strKey, String strIn) throws Exception {
+        SecretKeySpec secretKeySpec = getKey(strKey);
+        Cipher cipher = Cipher.getInstance(CIPHER);
+        IvParameterSpec iv = new IvParameterSpec(INITIAL_VECTOR.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, iv);
+        byte[] encrypted = cipher.doFinal(strIn.getBytes());
+        return new BASE64Encoder().encode(encrypted);
+    }
+
+    public static String decrypt(String strKey, String strIn) throws Exception {
+        SecretKeySpec secretKeySpec = getKey(strKey);
+        Cipher cipher = Cipher.getInstance(CIPHER);
+        IvParameterSpec iv = new IvParameterSpec(INITIAL_VECTOR.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, iv);
+        byte[] encrypted1 = new BASE64Decoder().decodeBuffer(strIn);
+
+        byte[] original = cipher.doFinal(encrypted1);
+        String originalString = new String(original);
+        return originalString;
+    }
+
+    private static SecretKeySpec getKey(String strKey) throws Exception {
+        byte[] tmpArray = strKey.getBytes();
+        byte[] array = new byte[16]; // 创建一个空的16位字节数组（默认值为0）
+
+        System.arraycopy(tmpArray, 0, array, 0, tmpArray.length > 16 ? 16 : tmpArray.length);
+        return new SecretKeySpec(array, "AES");
+    }
+
+    public static void main(String[] args) throws Exception {
+        String Code = "中文ABc123";
+        String key = "1q2w3e4r";
+        String codE;
+
+        codE = encrypt(key, Code);
+
+        System.out.println("原文：" + Code);
+        System.out.println("密钥：" + key);
+        System.out.println("密文：" + codE);
+        System.out.println("解密：" + decrypt(key, codE));
+    }
 }
 
 enum SslRole {
