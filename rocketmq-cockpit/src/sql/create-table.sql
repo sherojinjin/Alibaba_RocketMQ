c595c9fce54b5e0eb759b89fa878af7a7b63115b
Zhanhui Li <lizhanhui@gmail.com>
2014-12-24 15:15:39 +0800

    Edit DB scripts

--- rocketmq-cockpit/src/sql/create-table.sql
+++ rocketmq-cockpit/src/sql/create-table.sql
@@ -3,40 +3,46 @@ USE cockpit;
 CREATE TABLE IF NOT EXISTS name_server (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   ip INT NOT NULL,
-  port SMALLINT NOT NULL DEFAULT 9876
-);
+  port SMALLINT NOT NULL DEFAULT 9876,
+  create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time DATETIME NOT NULL DEFAULT 0
+) ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS ip_mapping(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   inner_ip INT NOT NULL,
-  public_ip INT NOT NULL
-);
+  public_ip INT NOT NULL,
+  create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time DATETIME NOT NULL DEFAULT 0
+) ENGINE = INNODB;
 
-CREATE TABLE IF NOT EXISTS topic(
+CREATE TABLE IF NOT EXISTS topic (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  cluster VARCHAR(100) NOT NULL DEFAULT 'DefaultCluster',
-  permission TINYINT NOT NULL DEFAULT 6
--- continue
-);
+  topic VARCHAR(255) NOT NULL,
+  cluster_name VARCHAR(100) NOT NULL DEFAULT 'DefaultCluster',
+  permission TINYINT NOT NULL DEFAULT 6,
+  write_queue_num INT NOT NULL DEFAULT 4,
+  read_queue_num INT NOT NULL DEFAULT 4,
+  unit BOOL NOT NULL DEFAULT FALSE ,
+  has_unit_subscription BOOL NOT NULL DEFAULT FALSE ,
+  broker_address VARCHAR(255),
+  `order` BOOL DEFAULT FALSE,
+  create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time DATETIME NOT NULL DEFAULT 0
+) ENGINE = INNODB;
 
 
-CREATE TABLE IF NOT EXISTS consumer_group(
+CREATE TABLE IF NOT EXISTS consumer_group (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  enable BOOL,
-  broadcasting BOOL
--- continue
-);
-
--- ------------------------------------------------------------
--- Constraints and indexes.
--- ------------------------------------------------------------
-
-
-
-
-
-
-
-
+  which_broker_when_consume_slowly INT NOT NULL DEFAULT 1,
+  group_name VARCHAR(255) NOT NULL,
+  consume_enable BOOL NOT NULL DEFAULT TRUE ,
+  consume_broadcast_enable BOOL NOT NULL DEFAULT FALSE ,
+  broker_address VARCHAR(255),
+  broker_id INT,
+  retry_max_times INT NOT NULL DEFAULT 3,
+  retry_queue_num MEDIUMINT NOT NULL DEFAULT 3,
+  consume_from_min_enable BOOL NOT NULL DEFAULT TRUE,
+  create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time DATETIME NOT NULL DEFAULT 0
+) ENGINE = INNODB;
