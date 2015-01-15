80ca94eddc2d10bea977ca3897783d543d4ffbff
Zhanhui Li <lizhanhui@gmail.com>
2015-01-15 10:52:01 +0800

    Update Example

--- rocketmq-babel/src/main/java/com/ndpmedia/rocketmq/babel/example/ProducerClient.java
+++ rocketmq-babel/src/main/java/com/ndpmedia/rocketmq/babel/example/ProducerClient.java
@@ -3,35 +3,26 @@ package com.ndpmedia.rocketmq.babel.example;
 import com.ndpmedia.rocketmq.babel.Message;
 import com.ndpmedia.rocketmq.babel.Producer;
 import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TNonblockingSocket;
-import org.apache.thrift.transport.TNonblockingTransport;
-
-import java.io.IOException;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
 
 public class ProducerClient {
+    private static final int PORT = Integer.parseInt(System.getProperty("RocketMQProducerPort", "10921"));
+
     public static void main(String[] args) throws TException {
-        TNonblockingTransport transport = null;
+        TTransport transport = null;
         try {
-            transport = new TNonblockingSocket("localhost", 3210);
-            transport.startConnect();
+            transport = new TSocket("localhost", PORT);
+            transport.open();
 
-
-            while (!transport.finishConnect()) {
-                Thread.sleep(100);
-            }
-
-            TProtocol protocol = new TCompactProtocol(transport);
+            TProtocol protocol = new TBinaryProtocol(transport);
             Producer.Client client = new Producer.Client(protocol);
             Message message = new Message();
             message.setTopic("T_QuickStart");
             message.setData("Test".getBytes());
             client.send(message);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         } finally {
             if (null != transport) {
                 transport.close();
