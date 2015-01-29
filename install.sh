023961db79583f99ef2cefc0e0a0b7a204c2cae8
golay <lizhanhui@gmail.com>
2015-01-29 11:05:55 +0800

    Add maven options.

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ if [ -z "$JAVA_HOME" ]; then
   JAVA_HOME=/opt/taobao/java
 fi
 export PATH=/opt/taobao/mvn/bin:$JAVA_HOME/bin:$PATH
+set MAVEN_OPTS=-Xms512M -Xmx512M -XX:MaxPermSize=256M -XX:PermSize=256M
 mvn -Dmaven.test.skip=true clean package install assembly:assembly -U
 
 ln -s target/alibaba-rocketmq-3.2.2/alibaba-rocketmq devenv
