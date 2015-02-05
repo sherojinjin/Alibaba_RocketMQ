d52c4f1ff3195401750c807f50be7145b0cfea40
Zhanhui Li <lizhanhui@gmail.com>
2015-02-05 16:39:18 +0800

    Update IP

--- rocketmq-cockpit/src/sql/insert-data.sql
+++ rocketmq-cockpit/src/sql/insert-data.sql
@@ -6,16 +6,16 @@ INSERT INTO name_server(ip, port) VALUES ('54.173.209.191', 9876);
 
 
 -- Insert IP mapping.
-INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.5.36.11', '54.94.203.40');
-INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.1.36.13', '54.174.104.250');
-INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.2.36.10', '54.169.194.51');
-INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.1.36.10', '54.173.39.198');
-INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.2.36.11', '54.169.174.172');
-INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.3.36.10', '54.67.6.98');
-INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.5.36.10', '54.94.212.186');
-INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.3.36.11', '54.67.77.111');
-INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.1.36.12', '54.174.184.203');
-INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.1.36.11', '54.173.209.191');
+INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.5.36.11',	'54.94.189.195');
+INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.1.36.13',	'52.0.2.7');
+INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.1.36.10',	'54.173.39.198');
+INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.2.36.11',	'54.169.36.156');
+INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.3.36.10',	'54.67.33.141');
+INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.5.36.10',	'54.94.158.107');
+INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.3.36.11',	'54.67.112.198');
+INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.1.36.12',	'52.0.87.252');
+INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.1.36.11',	'54.173.209.191');
+INSERT INTO ip_mapping(inner_ip, public_ip) VALUES ('10.2.36.10',	'54.179.161.99');
 
 INSERT INTO cockpit_user(username, password, role) VALUES ('root', '320734fbb627d6884a1284acbdaa5db9', 'ROLE_ADMIN');
 INSERT INTO cockpit_user(username, password, role) VALUES ('xutao', '23dc3038d25ef09f3ad7c0552b40ef9f', 'ROLE_USER');
