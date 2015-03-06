c6e0af83c5b15f72e7cf2cdee2b47fa0d24b414f
Xingwei Ruan <xingweiruan@gmail.com>
2015-03-06 14:59:47 +0800

    Fix typo.

--- rocketmq-babel/startup.sh
+++ rocketmq-babel/startup.sh
@@ -12,7 +12,7 @@ if [ x"$1" != x ]; then
 fi
 
 #JAVA_OPTS="-Duser.timezone=GMT+8 -server -Xms512m -Xmx512m -Xloggc:$APP_LOG/producerServer.gc.log -Denable_ssl=true -Drocketmq.namesrv.domain=172.30.50.54 "
-JAVA_OPTS=" -Duser.timezone=GMT+8 -server -Xms512m -Xmx512m -Xloggc:$APP_LOG/producerServer.gc.log -Denable_ssl=true -DocketMQClientPassword=4R4MNv54OnDeoHSRnngFkA== -Dlog.home=$APP_LOG "
+JAVA_OPTS=" -Duser.timezone=GMT+8 -server -Xms512m -Xmx512m -Xloggc:$APP_LOG/producerServer.gc.log -Denable_ssl=true -DRocketMQClientPassword=VVYZZ9NLVdy849XIy/tM3Q== -Dlog.home=$APP_LOG "
 APP_MAIN=com.ndpmedia.rocketmq.babel.$server
 CLASSPATH=target/classes
 echo $CLASSPATH
