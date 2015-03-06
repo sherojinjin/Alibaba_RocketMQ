7544ff4e87ee284d53ff35327081050de094fc8a
penuel-leo <penuel.leo@gmail.com>
2015-03-06 10:20:47 +0800

    online config

--- rocketmq-babel/startup.sh
+++ rocketmq-babel/startup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #JAVA_HOME="/usr/java/jdk1.6.0_31"
-APP_LOG=logs
+APP_LOG=/home/dev/dumps/logs
 if [ ! -d "$APP_LOG" ]; then
-  mkdir $APP_LOG
+  mkdir $APP_LOG -p
 fi
-JAVA_OPTS="-Duser.timezone=GMT+8 -server -Xms512m -Xmx512m -Xloggc:logs/gc.log -Denable_ssl=true -Drocketmq.namesrv.domain=172.30.50.54 "
+#JAVA_OPTS="-Duser.timezone=GMT+8 -server -Xms512m -Xmx512m -Xloggc:$APP_LOG/producerServer.gc.log -Denable_ssl=true -Drocketmq.namesrv.domain=172.30.50.54 "
+JAVA_OPTS=" -Duser.timezone=GMT+8 -server -Xms512m -Xmx512m -Xloggc:$APP_LOG/producerServer.gc.log -Denable_ssl=true -DocketMQClientPassword=4R4MNv54OnDeoHSRnngFkA== -Dlog.home=$APP_LOG "
 APP_MAIN=com.ndpmedia.rocketmq.babel.$1
 CLASSPATH=target/classes
 echo $CLASSPATH
