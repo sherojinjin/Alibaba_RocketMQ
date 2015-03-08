214cfd122eae9f1b52384d4f0507c8cafdaf1ca4
Xingwei Ruan <xingweiruan@gmail.com>
2015-03-08 20:58:00 +0800

    Apply kv to ns.

--- rocketmq-cockpit/src/main/java/com/ndpmedia/rocketmq/cockpit/controller/NameServerController.java
+++ rocketmq-cockpit/src/main/java/com/ndpmedia/rocketmq/cockpit/controller/NameServerController.java
@@ -1,11 +1,16 @@
 package com.ndpmedia.rocketmq.cockpit.controller;
 
+import com.alibaba.rocketmq.client.exception.MQBrokerException;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.remoting.exception.RemotingException;
+import com.alibaba.rocketmq.tools.admin.DefaultMQAdminExt;
 import com.ndpmedia.rocketmq.nameserver.NameServerKVService;
 import com.ndpmedia.rocketmq.nameserver.model.KV;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -44,4 +49,12 @@ public class NameServerController {
         return "forward:/cockpit/name-server/kv";
     }
 
+    @RequestMapping(value = "/kv/{id}", method = RequestMethod.GET)
+    public String apply(@PathVariable("id")long id)
+            throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
+        KV kv = nameServerKVService.get(id);
+        DefaultMQAdminExt mqAdmin = new DefaultMQAdminExt();
+        mqAdmin.createAndUpdateKvConfig(kv.getNameSpace(), kv.getKey(), kv.getValue());
+        return "forward:/cockpit/name-server/kv";
+    }
 }
