4aa4f62bce09fe28275313c8e3e1cf6073665134
penuel-leo <penuel.leo@gmail.com>
2015-03-11 14:51:58 +0800

    startup.sh

--- rocketmq-babel/startup.sh
+++ rocketmq-babel/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #JAVA_HOME="/usr/java/jdk1.6.0_31"
-APP_LOG=/dianyi/app/justniffer/logs
+APP_LOG=/dianyi/app/justniffer
 if [ ! -d "$APP_LOG" ]; then
   mkdir $APP_LOG -p
 fi
@@ -12,7 +12,7 @@ if [ x"$1" != x ]; then
 fi
 
 #JAVA_OPTS="-Duser.timezone=GMT+8 -server -Xms512m -Xmx512m -Xloggc:$APP_LOG/producerServer.gc.log -Denable_ssl=true -Drocketmq.namesrv.domain=172.30.50.54 "
-JAVA_OPTS=" -Duser.timezone=GMT+8 -server -Xms512m -Xmx512m -Xloggc:$APP_LOG/producerServer.gc.log -Denable_ssl=true -DocketMQClientPassword=4R4MNv54OnDeoHSRnngFkA== -Dlog.home=$APP_LOG "
+JAVA_OPTS=" -Duser.timezone=GMT+8 -server -Xms512m -Xmx512m -Xloggc:$APP_LOG/logs/producerServer.gc.log -Denable_ssl=true -DRocketMQClientPassword=4R4MNv54OnDeoHSRnngFkA== -Dlog.home=$APP_LOG "
 APP_MAIN=com.ndpmedia.rocketmq.babel.$server
 CLASSPATH=target/classes
 echo $CLASSPATH
@@ -26,4 +26,4 @@ done
 echo $LIB_JARS
 JAVA_HOME=$JAVA_HOME
 if [ ! -d "$JAVA_HOME" ]; then JAVA_HOME="/usr"; else echo "$JAVA_HOME"; fi;
-nohup $JAVA_HOME/bin/java $JAVA_OPTS -Dworkdir=./  -classpath target/classes:$LIB_JARS $APP_MAIN > $APP_LOG/$server.nohup.log &
+nohup $JAVA_HOME/bin/java $JAVA_OPTS -Dworkdir=./  -classpath target/classes:$LIB_JARS $APP_MAIN > $APP_LOG/logs/$server.nohup.log &
