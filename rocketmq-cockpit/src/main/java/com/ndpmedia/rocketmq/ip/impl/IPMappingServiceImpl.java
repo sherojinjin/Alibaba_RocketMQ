cd64e9b3ca74dcecb8909dea0bbc5409698282e8
Xingwei Ruan <xingweiruan@gmail.com>
2015-03-12 17:37:10 +0800

    Refactor.

--- rocketmq-cockpit/src/main/java/com/ndpmedia/rocketmq/ip/impl/IPMappingServiceImpl.java
+++ rocketmq-cockpit/src/main/java/com/ndpmedia/rocketmq/ip/impl/IPMappingServiceImpl.java
@@ -13,8 +13,4 @@ public class IPMappingServiceImpl implements IPMappingService {
     public String lookUp(String innerIP) {
         return ipPairMapper.lookUp(innerIP);
     }
-
-    public static void main(String[] args) {
-        System.out.println(System.currentTimeMillis());
-    }
 }
